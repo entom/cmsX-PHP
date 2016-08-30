d1e35bf4e547774fa0cb8aef1c9cbb35be733ec5
tomasz <tomasznicieja@gmail.com>
2016-08-30 22:16:29 +0200

    rendering list of brands and categories

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -5,7 +5,21 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col s12">
+            <div class="col s12 m3">
+                <h5>Brands</h5>
+                <ul>
+                    @foreach($brands as $brand)
+                        <li>{{$brand->name}}</li>
+                    @endforeach
+                </ul>
+                <h5>Categories</h5>
+                <ul>
+                    @foreach($categories as $category)
+                        <li>{{$category->name}}</li>
+                    @endforeach
+                </ul>
+            </div>
+            <div class="col s12 m9">
                 <h1>{{$title}}</h1>
             </div>
         </div>
