8451a85c6b89e6f682737d64dea89d4d9624bbb4
tnicieja <tomasznicieja@gmail.com>
2016-09-24 10:40:19 +0200

    changing active entry blog to select

--- resources/views/admin/blogs/edit.blade.php
+++ resources/views/admin/blogs/edit.blade.php
@@ -25,7 +25,7 @@
 
                             <div class="input-field col s12 m6 {{ $errors->has('active') ? 'has-error' : ''}}">
                                 {!! $errors->first('active', '<p class="help-block">:message</p>') !!}
-                                {!! Form::text('active', null, []) !!}
+                                {!! Form::select('active', [1 => 'Tak', 0 => 'Nie']) !!}
                                 {!! Form::label('active', 'Aktywny', ['data-error' => 'wrong', 'data-success' => 'right']) !!}
                             </div>
 
