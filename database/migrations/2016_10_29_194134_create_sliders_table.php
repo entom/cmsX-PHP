4226d9fd49d777f0cf811a0bb053130dfcc9bef0
tnicieja <tomasznicieja@gmail.com>
2016-10-29 21:57:48 +0200

    migration with sliders table

---
+++ database/migrations/2016_10_29_194134_create_sliders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSlidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 255);
+            $table->string('file', 255);
+            $table->integer('position')->default(0)->nullable();
+            $table->string('link', 1024);
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sliders');
+    }
+}
