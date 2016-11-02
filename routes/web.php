45dcc5196a0de4db0278f1255a454920be5ef795
tnicieja <tomasznicieja@gmail.com>
2016-11-02 23:07:28 +0100

    routing for paralax admin module

--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,7 @@ Route::resource('admin/news', 'Admin\\NewsController');
 Route::resource('admin/albums', 'Admin\\AlbumsController');
 Route::resource('admin/logos', 'Admin\\LogosController');
 Route::resource('admin/sliders', 'Admin\\SlidersController');
+Route::resource('admin/paralax', 'Admin\\ParalaxController');
 
 Route::resource('api/calendar-event-category', 'Api\\CalendarEventCategoryController');
 
