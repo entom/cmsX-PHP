2810c129f46cc7076a8e0b24545564f4dc7ad810
tnicieja <tomasznicieja@gmail.com>
2016-11-11 21:31:42 +0100

    admin menu update with realization item

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -232,6 +232,24 @@
                         <li class="collapsible__element">
                             <a class="collapsible-header">
                                 <sapn class="icon__container">
+                                    <i class="fa fa-briefcase" aria-hidden="true"></i>
+                                </sapn>
+                                <span>Realizacje</span>
+                            </a>
+                            <div class="collapsible-body">
+                                <ul>
+                                    <li><a href="{{url('/admin/realizations')}}">Lista</a></li>
+                                    <li><a href="{{url('/admin/realizations/create')}}">Nowy wpis</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <li class="no-padding">
+                    <ul class="collapsible collapsible-accordion">
+                        <li class="collapsible__element">
+                            <a class="collapsible-header">
+                                <sapn class="icon__container">
                                     <i class="fa fa-users" aria-hidden="true"></i>
                                 </sapn>
                                 <span>Użytkownicy</span>
