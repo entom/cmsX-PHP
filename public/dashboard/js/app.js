7d981deec29d417697d28357cf9021382466d463
tnicieja <tomasznicieja@gmail.com>
2016-11-12 14:21:54 +0100

    angular-upload in app.js

--- public/dashboard/js/app.js
+++ public/dashboard/js/app.js
@@ -2,7 +2,7 @@
  * Created by tomasz on 03.09.2016.
  */
 
-var cmsx = angular.module('cmsxApp', ['ui.calendar'], function($interpolateProvider) {
+var cmsx = angular.module('cmsxApp', ['ui.calendar', 'angularFileUpload'], function($interpolateProvider) {
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
 });
