db52e871b8b27b7d1466371ba2b8334dc268188e
tnicieja <tomasznicieja@gmail.com>
2016-11-12 14:19:05 +0100

    migration file for photos table

---
+++ database/migrations/2016_11_12_131304_create_photos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('album_id');
+            $table->string('title', 255)->nullable()->default(null);
+            $table->integer('position')->nullable()->default(0);
+            $table->string('file', 255)->nullable()->default(null);
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('photos');
+    }
+}
