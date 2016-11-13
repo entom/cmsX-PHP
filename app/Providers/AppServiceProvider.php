08a618ef77f10ce8205b8c4595a495f224e83f68
tnicieja <tomasznicieja@gmail.com>
2016-11-13 23:14:26 +0100

    giving controller and action names to all views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
     {
         view()->composer('*', function ($view)
         {
+            $action = app('request')->route()->getAction();
+            $controller = class_basename($action['controller']);
+            list($controller, $action) = explode('@', $controller);
+            $view->with(compact('controller', 'action'));
+
             if(Auth::check())
             {
                 if(Auth::user()->admin == 1 || Auth::user()->admin == true)
