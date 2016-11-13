1975116cafcc3a7cfcb85b50f70e190f78098846
tnicieja <tomasznicieja@gmail.com>
2016-11-13 20:41:04 +0100

    function for preparing date/time fields

--- app/Model/BaseModel.php
+++ app/Model/BaseModel.php
@@ -29,4 +29,24 @@ class BaseModel extends Model
         DB::statement($sql);
     }
 
+    /**
+     * convertCreatedDates method
+     * @param $object
+     */
+    public static function convertCreatedDates(&$object)
+    {
+        $object->created_date = date('Y-m-d', strtotime($object->created_at));
+        $object->created_time = date('H:i:s', strtotime($object->created_at));
+    }
+
+    /**
+     * convertCreatedDates method
+     * @param $object
+     */
+    public static function convertUpdatedDates(&$object)
+    {
+        $object->updated_date = date('Y-m-d', strtotime($object->updated_at));
+        $object->updated_time = date('H:i:s', strtotime($object->updated_at));
+    }
+
 }
