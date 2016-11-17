d77adfc7181e16034ecd68e1618fb7c38ffbf65b
Tomasz Nicieja <tomasznicieja@MacBook-Pro-Tomasz.local>
2016-11-17 21:57:55 +0100

    making SEO tab extendable to create/edit in many modules

--- resources/views/admin/offer/create.blade.php
+++ resources/views/admin/offer/create.blade.php
@@ -14,7 +14,7 @@
             <div class="col s12">
                 <ul class="tabs">
                     <li class="tab col s3"><a class="active" href="#contentTab">Treść</a></li>
-                    <li class="tab col s3"><a class="active" href="#seoTab">SEO</a></li>
+                    @include('admin.partials.seo_tab')
                 </ul>
             </div>
             <div id="contentTab" class="col s12">
@@ -63,34 +63,7 @@
                     </div>
                 </div>
             </div>
-            <div id="seoTab" class="col s12">
-                <div class="card">
-                    <div class="card__header">
-                        <span>SEO - Meta dane</span>
-                    </div>
-                    <div class="card-content">
-                        <div class="row">
-                            <div class="input-field col s12 m6 {{ $errors->has('meta_title') ? 'has-error' : ''}}">
-                                {!! $errors->first('meta_title', '<p class="help-block">:message</p>') !!}
-                                {!! Form::text('meta_title', null, []) !!}
-                                {!! Form::label('meta_title', 'Tytuł', ['data-error' => 'wrong', 'data-success' => 'right']) !!}
-                            </div>
-
-                            <div class="input-field col s12 m6 {{ $errors->has('meta_keywords') ? 'has-error' : ''}}">
-                                {!! $errors->first('meta_keywords', '<p class="help-block">:message</p>') !!}
-                                {!! Form::text('meta_keywords', null, []) !!}
-                                {!! Form::label('meta_keywords', 'Słowa kluczowe', ['data-error' => 'wrong', 'data-success' => 'right']) !!}
-                            </div>
-
-                            <div class="input-field col s12 m12 {{ $errors->has('meta_description') ? 'has-error' : ''}}">
-                                {!! $errors->first('meta_description', '<p class="help-block">:message</p>') !!}
-                                {!! Form::text('meta_description', null, []) !!}
-                                {!! Form::label('meta_description', 'Opis', ['data-error' => 'wrong', 'data-success' => 'right']) !!}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @include('admin.partials.seo_tab_content')
         </div>
 
         <div class="col s12 m12">
