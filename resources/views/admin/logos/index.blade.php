28ba32d464033d17c3067a33b41b52d6f491eec3
Tomasz Nicieja <tomasznicieja@MacBook-Pro-Tomasz.local>
2016-11-19 11:04:49 +0100

    remove local csrf token verify

--- resources/views/admin/logos/index.blade.php
+++ resources/views/admin/logos/index.blade.php
@@ -79,14 +79,4 @@
             </div>
         </div>
     </div>
-
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
-
-    <script>
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-    </script>
 @endsection
